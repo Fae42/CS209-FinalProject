7b29a7374ca11eb02f028bdd556ccca38f217396
Fae42 <Fae_@outlook.com>
2023-01-03 23:54:55 +0800

    checkstyle fix

--- src/main/java/com/example/springproject/service/RepoServiceImpl.java
+++ src/main/java/com/example/springproject/service/RepoServiceImpl.java
@@ -23,18 +23,18 @@ public class RepoServiceImpl implements RepoService {
     public void updateInfo(String owner, String repoName) {
         System.out.println("update repo, may take long time and crash due to GitHub API limits");
         Repo repo = findByOwnerAndName(owner, repoName);
-        if (repo==null) {
+        if (repo == null) {
             repo = new Repo();
             repo.setOwner(owner);
             repo.setName(repoName);
             repoRepository.save(repo);
         }
-        long repoID = repo.getId();
+        long repoId = repo.getId();
         
-        developerService.update(owner, repoName, repoID);
-        issueService.update(owner, repoName, repoID);
-        releaseService.update(owner, repoName, repoID);
-        commitService.update(owner, repoName, repoID);
+        developerService.update(owner, repoName, repoId);
+        issueService.update(owner, repoName, repoId);
+        releaseService.update(owner, repoName, repoId);
+        commitService.update(owner, repoName, repoId);
     }
     
     @Override
